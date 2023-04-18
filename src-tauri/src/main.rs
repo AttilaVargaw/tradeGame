455e33501c21afd66460230ce62362667a4f0e8d
AttilaVargaw <attilavargaw@gmail.com>
2023-04-18 05:57:18 +0200

    Update tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,15 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
 #[tauri::command]
 fn my_custom_command(argument: String) {
-  println!("{}", argument);
+    println!("{}", argument);
 }
 
 fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_sql::Builder::default().build())
-    .invoke_handler(tauri::generate_handler![my_custom_command])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder
+        ::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![my_custom_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
